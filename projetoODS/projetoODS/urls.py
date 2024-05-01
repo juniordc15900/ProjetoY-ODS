c1620d4fb196dbe8dca205b0e57bf7ae384a4f41
João Antônio <joaoasccc@gmail.com>
2024-04-30 21:38:18 -0300

    feat: atualização requirements

--- projetoODS/projetoODS/urls.py
+++ projetoODS/projetoODS/urls.py
@@ -5,10 +5,11 @@ from . import views
 urlpatterns = [
     path("app/", include("app.urls")),
     path('admin/', admin.site.urls),
-    path('', views.home, name="index"),
-    path('login/', views.signIn),
-    path('postsignIn/', views.postsignIn),
-    path('signUp/', views.signUp, name="signup"),
-    path('logout/', views.logout, name="log"),
-    path('postsignUp/', views.postsignUp),
+    path(' ', views.home, name="index"),
+    path('/login/', views.signIn),
+    path('/postsignIn/', views.postsignIn),
+    path('/signUp/', views.signUp, name="signup"),
+    path('/logout/', views.logout, name="log"),
+    path('/postsignUp/', views.postsignUp),
 ]
+
