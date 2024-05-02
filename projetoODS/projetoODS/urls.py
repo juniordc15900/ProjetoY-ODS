9e0a5aae479527aabd686f75768fc733da7e1776
João Antônio <joaoasccc@gmail.com>
2024-05-02 11:25:23 -0300

    update urls

--- projetoODS/projetoODS/urls.py
+++ projetoODS/projetoODS/urls.py
@@ -9,10 +9,10 @@ urlpatterns = [
     path('gemini/', include('gemini.urls')),
 
     path(' ', views.home, name="index"),
-    path('/login/', views.signIn),
-    path('/postsignIn/', views.postsignIn),
-    path('/signUp/', views.signUp, name="signup"),
-    path('/logout/', views.logout, name="log"),
-    path('/postsignUp/', views.postsignUp),
+    path('login/', views.signIn),
+    path('postsignIn/', views.postsignIn),
+    path('signUp/', views.signUp, name="signup"),
+    path('logout/', views.logout, name="log"),
+    path('postsignUp/', views.postsignUp),
 ]
 
