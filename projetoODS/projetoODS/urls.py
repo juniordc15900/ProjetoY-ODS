062301a989f2d9a2209aef12bc1b840864c868d2
João Antônio <joaoasccc@gmail.com>
2024-05-06 11:29:20 -0300

    rowback urls

--- projetoODS/projetoODS/urls.py
+++ projetoODS/projetoODS/urls.py
@@ -3,16 +3,12 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', views.index, name='index'),
-    path('godaddy/', include('godaddy.urls')),
-    path('gemini/', include('gemini.urls')),
-
-    path(' ', views.home, name="index"),
+    path("app/", include("app.urls")),
+	path('admin/', admin.site.urls),
+	path(' ', views.home, name ="index"),
     path('login/', views.signIn),
     path('postsignIn/', views.postsignIn),
     path('signUp/', views.signUp, name="signup"),
     path('logout/', views.logout, name="log"),
     path('postsignUp/', views.postsignUp),
 ]
-
