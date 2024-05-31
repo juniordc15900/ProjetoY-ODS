9f427d4101f2c520a009069163f8fb5e108667ae
Felipao98 <acog24@gmail.com>
2024-05-31 13:52:17 -0300

    implementei uma classe no modelo app

--- projetoODS/gemini/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path("gemini-dominios", views.gemini_dominios, name="gemini-dominios"),
-    path("teste", views.teste, name="teste"),
-]
